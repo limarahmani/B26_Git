21135f124a1202a57e1fb51053196924590f6c1b
limar <limarahmani321@gmail.com>
2022-03-29 16:17:51 -0700

    updated first class

--- src/day0/First.java
+++ src/day0/First.java
@@ -4,5 +4,7 @@ public class First {
     public static void main(String[] args) {
         System.out.println("Learning Git");
         //this is fun
+        // this is not too much fun
+        System.out.println("Git is good");
     }
 }
