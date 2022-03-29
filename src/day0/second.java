7de4b8afd84292cba10d2494fbfbeb09e6dd0dea
limar <limarahmani321@gmail.com>
2022-03-29 16:42:17 -0700

    updated second class

--- src/day0/second.java
+++ src/day0/second.java
@@ -3,5 +3,14 @@ package day0;
 public class second {
     public static void main(String[] args) {
         // I don't know what to write
+        //Are there no Bruce Willis fans out there? The bucket question is actually from Die Hard 🙂
+        //
+        //*Fill the 5 liter can from the tap
+        //*Empty the 5 liter can into the 3 liter can - leaving 2 liter in the 5 liter can.
+        //*Pour away the contents of the 3 liter can.
+        //*Fill the 3 liter can with the 2 liter from the 5 liter can - leaving 2 liters in the 3 liters can.
+        //*Fill the 5 liters can from the tap.
+        //*Fill the remaining 1 liter space in the 3 liters can from the 5 liters can.
+        //*Leaving 4 liters in the 5 liter can.
     }
 }
